fef86969d524f3500a44e318aed7569b570dbfe5
pprasko <prasinar@gmail.com>
2021-06-22 00:05:22 +0200

    Production build, 2nd version

---
+++ precache-manifest.6f1e5f2de4b291a317631eaea97c6670.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd7f3c5560d967fc7dc5ba822b4122a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d46bc7f998630633cfc",
+    "url": "./static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "d2c4c13990b57275edf5",
+    "url": "./static/css/main.18360df6.chunk.css"
+  },
+  {
+    "revision": "7d46bc7f998630633cfc",
+    "url": "./static/js/2.e9a019b3.chunk.js"
+  },
+  {
+    "revision": "02067fd0c83bdf6c1647052222ee28ce",
+    "url": "./static/js/2.e9a019b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c4c13990b57275edf5",
+    "url": "./static/js/main.8ffa1196.chunk.js"
+  },
+  {
+    "revision": "5d0b30973901cb05f4e0",
+    "url": "./static/js/runtime-main.0d92c038.js"
+  },
+  {
+    "revision": "02dfcd657b2de8baf47c047fc38c8d55",
+    "url": "./static/media/gradient1.02dfcd65.png"
+  },
+  {
+    "revision": "b883fd4d5ba3e3a369da439161c23f3a",
+    "url": "./static/media/logo192.b883fd4d.png"
+  }
+]);
